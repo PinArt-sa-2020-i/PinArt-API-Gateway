006254d6c89006b2b1cad18cd2c93e53e6c77e1d
tpereza <tpereza@unal.edu.co>
2020-04-19 17:09:48 -0500

    :sparkles::boom: MS-Profile

--- src/dataSources/profile_api.js
+++ src/dataSources/profile_api.js
@@ -24,19 +24,56 @@ class ProfileAPI extends RESTDataSource {
 
   async createUser(user) {
     user = new Object(JSON.parse(JSON.stringify(user)));
-    const response = await this.post("users", user);
+    const response = await this.post("user", user);
     return this.userReducer(response);
   }
 
   async updateUser(id, user) {
     user = new Object(JSON.parse(JSON.stringify(user)));
-    const response = await this.put(`users/${id}`, user);
+    const response = await this.put(`user/${id}`, user);
     return this.userReducer(response);
   }
 
-  async deleteUser(id) {
-    const response = await this.delete(`user/${id}`);
+  async deleteUser(id, user) {
+    user = new Object(JSON.parse(JSON.stringify(user)));
+    const response = await this.put(`user/${id}`, user);
     return this.userReducer(response);
+  }
+
+  async createAuth(auth) {
+    auth = new Object(JSON.parse(JSON.stringify(auth)));
+    const response = await this.post("auth", auth);
+    return this.authReducer(response);
+  }
+
+  async createProfile(profile) {
+    profile = new Object(JSON.parse(JSON.stringify(profile)));
+    const response = await this.post("profile", profile);
+    return this.authReducer(response);
+  }
+
+  async createCountry(country) {
+    country = new Object(JSON.parse(JSON.stringify(country)));
+    const response = await this.post("country", country);
+    return this.authReducer(response);
+  }
+
+  async createRecovery(recovery) {
+    recovery = new Object(JSON.parse(JSON.stringify(recovery)));
+    const response = await this.post("recovery", recovery);
+    return this.authReducer(response);
+  }
+
+  async createReport(report) {
+    report = new Object(JSON.parse(JSON.stringify(report)));
+    const response = await this.post("report", report);
+    return this.authReducer(response);
+  }
+
+  async createCause(cause) {
+    cause = new Object(JSON.parse(JSON.stringify(cause)));
+    const response = await this.post("cause", cause);
+    return this.authReducer(response);
   }
 
   //Le da el formato necesario a la salida
@@ -50,6 +87,69 @@ class ProfileAPI extends RESTDataSource {
       eliminado: user.eliminado,
       privado: user.privado,
       createdDate: user.createdDate,
+      auth: user.auth,
+      profiles: user.profiles,
+      recoveries: user.recoveries,
+      received: user.received,
+    };
+  }
+
+  authReducer(auth) {
+    return {
+      id: auth.id,
+      estado: auth.estado,
+      userId: auth.userId,
+    };
+  }
+
+  profileReducer(profile) {
+    return {
+      country: profile.country,
+      id: profile.id,
+      fechaNacimiento: profile.fechaNacimiento,
+      genero: profile.genero,
+      foto: profile.foto,
+      descripcion: profile.descripcion,
+      noTelefono: profile.noTelefono,
+      edad: profile.edad,
+      userId: profile.userId,
+      countryId: profile.countryId,
+    };
+  }
+
+  countryReducer(country) {
+    return {
+      id: country.id,
+      nombre: country.nombre,
+      prefijo: country.prefijo,
+    };
+  }
+
+  recoveryReducer(recovery) {
+    return {
+      id: recovery.id,
+      recoveryCode: recovery.recoveryCode,
+      used: recovery.used,
+      createdDate: recovery.createdDate,
+      userId: recovery.userId,
+    };
+  }
+
+  reportReducer(report) {
+    return {
+      cause: report.cause,
+      id: report.id,
+      comentario: report.comentario,
+      userId: report.userId,
+      causeId: report.causeId,
+    };
+  }
+
+  causeReducer(cause) {
+    return {
+      id: cause.id,
+      nombre: cause.nombre,
+      descripcion: cause.descripcion,
     };
   }
 }
--- src/typeDefs/profile_type_def.js
+++ src/typeDefs/profile_type_def.js
@@ -10,6 +10,57 @@ const profileTypeDefs = gql`
     eliminado: Boolean!
     privado: Boolean!
     createdDate: String!
+    auth: [Auth]
+    profiles: [Profile]
+    recoveries: [Recovery]
+    received: [Report]
+  }
+
+  type Auth {
+    id: Int!
+    estado: String!
+    userId: Int!
+  }
+
+  type Profile {
+    country: Country
+    id: Int!
+    fechaNacimiento: String
+    genero: String
+    foto: String
+    descripcion: String
+    noTelefono: String
+    edad: String
+    userId: Int!
+    countryId: Int!
+  }
+
+  type Country {
+    id: Int!
+    nombre: String!
+    prefijo: String!
+  }
+
+  type Recovery {
+    id: Int!
+    recoveryCode: String!
+    used: Boolean!
+    createdDate: String!
+    userId: Int!
+  }
+
+  type Report {
+    cause: Cause!
+    id: Int!
+    comentario: String
+    userId: Int!
+    causeId: Int!
+  }
+
+  type Cause {
+    id: Int!
+    nombre: String!
+    descripcion: String!
   }
 
   input UserInput {
@@ -21,6 +72,44 @@ const profileTypeDefs = gql`
     privado: Boolean!
   }
 
+  input AuthInput {
+    estado: String!
+    userId: Int!
+  }
+
+  input ProfileInput {
+    fechaNacimiento: String
+    genero: String
+    foto: String
+    descripcion: String
+    noTelefono: String
+    edad: String
+    userId: Int!
+    countryId: Int!
+  }
+
+  input CountryInput {
+    nombre: String!
+    prefijo: String!
+  }
+
+  input RecoveryInput {
+    recoveryCode: String!
+    used: Boolean!
+    userId: Int!
+  }
+
+  input ReportInput {
+    comentario: String
+    userId: Int!
+    causeId: Int!
+  }
+
+  input CauseInput {
+    nombre: String!
+    descripcion: String!
+  }
+
   extend type Query {
     allUsers: [User]!
     userById(id: Int!): User!
@@ -28,6 +117,12 @@ const profileTypeDefs = gql`
 
   extend type Mutation {
     createUser(user: UserInput!): User!
+    createAuth(auth: AuthInput!): Auth!
+    createProfile(profile: ProfileInput!): Profile!
+    createCountry(country: CountryInput!): Country!
+    createRecovery(recovery: RecoveryInput!): Recovery!
+    createReport(report: ReportInput!): Report!
+    createCause(cause: CauseInput!): Cause!
     updateUser(id: Int!, user: UserInput!): User!
     deleteUser(id: Int!): Int
   }
