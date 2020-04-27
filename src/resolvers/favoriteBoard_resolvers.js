f847db2a3263481d0eebce7a9d810de90634f345
CamiloBallen24 <andrescamilo468@gmail.com>
2020-04-26 23:25:41 -0500

    :fire: Final Protected

--- src/resolvers/favoriteBoard_resolvers.js
+++ src/resolvers/favoriteBoard_resolvers.js
@@ -1,38 +1,111 @@
+const verficateAuthentication = (data) => {if(data.id == null){ throw new ApolloError("Need Authentication", 400);}}
+
 //Crea los resolver de example
 const favoriteBoardResolvers = {
     Query: {
-        allUser: (_, __, { dataSources }) => dataSources.favoriteboardAPI.getAllUser(),
-        allBoard: (_, __, { dataSources }) => dataSources.favoriteboardAPI.getAllBoard(),
-        allUserFollow: (_, __, { dataSources }) => dataSources.favoriteboardAPI.getAllUserFollow(),
-        allBoardFollow: (_, __, { dataSources }) => dataSources.favoriteboardAPI.getAllBoardFollow(),
+        allUser: async (_, __, { dataSources, data }) => {
+            verficateAuthentication(data)
+            return await dataSources.favoriteboardAPI.getAllUser()
+        },
 
-        userByIdBoards: (_, { id }, {dataSources}) => dataSources.favoriteboardAPI.getUserbyId(id),
-        usersFollowingByFollower: (_, { follower_id }, {dataSources}) => dataSources.favoriteboardAPI.getUsersFollowingByFollower(follower_id),
-        allBoardsByUser:(_, { id }, {dataSources}) => dataSources.favoriteboardAPI.getAllBoardsByUser(id),
-        boardById: (_, { id }, {dataSources}) => dataSources.favoriteboardAPI.getBoardbyId(id),
-        userFollowById: (_, { id }, {dataSources}) => dataSources.favoriteboardAPI.getUserFollowbyId(id),
-        boardFollowById: (_, { id }, {dataSources}) => dataSources.favoriteboardAPI.getBoardFollowbyId(id),
-        allBoardsByName:(_, { name }, {dataSources}) => dataSources.favoriteboardAPI.getAllBoardsByName(name),
+        allBoard: async (_, __, { dataSources, data }) => {
+            verficateAuthentication(data)
+            return await dataSources.favoriteboardAPI.getAllBoard()
+        },
+
+        allUserFollow: async (_, __, { dataSources, data }) => {
+            verficateAuthentication(data)
+            return await dataSources.favoriteboardAPI.getAllUserFollow()
+        },
+
+        allBoardFollow: async (_, __, { dataSources, data }) => {
+            verficateAuthentication(data)
+            return await dataSources.favoriteboardAPI.getAllBoardFollow()
+        },
+
+        userByIdBoards: async (_, { id }, {dataSources, data}) => {
+            verficateAuthentication(data)
+            return await dataSources.favoriteboardAPI.getUserbyId(id)
+        },
+
+        usersFollowingByFollower: async (_, { follower_id }, {dataSources, data}) => {
+            verficateAuthentication(data)
+            return await dataSources.favoriteboardAPI.getUsersFollowingByFollower(follower_id)
+        },
+
+        allBoardsByUser:async (_, { id }, {dataSources, data}) => {
+            verficateAuthentication(data)
+            return await dataSources.favoriteboardAPI.getAllBoardsByUser(id)
+        },
+
+        boardById: async (_, { id }, {dataSources, data}) => {
+            verficateAuthentication(data)
+            return await dataSources.favoriteboardAPI.getBoardbyId(id)
+        },
+
+        userFollowById: async (_, { id }, {dataSources, data}) => {
+            verficateAuthentication(data)
+            return await dataSources.favoriteboardAPI.getUserFollowbyId(id)
+        },
+
+        boardFollowById: async (_, { id }, {dataSources, data}) => {
+            verficateAuthentication(data)
+            return await dataSources.favoriteboardAPI.getBoardFollowbyId(id)
+        },
+
+        allBoardsByName:async (_, { name }, {dataSources, data}) => {
+            verficateAuthentication(data)
+            return await dataSources.favoriteboardAPI.getAllBoardsByName(name)
+        },
 
 
     },
 
     Mutation: {
-        createUserBoards: (_, {id}, {dataSources}) => dataSources.favoriteboardAPI.createUser(id),
-        deleteUserBoards: (_, {id}, {dataSources}) => dataSources.favoriteboardAPI.deleteUser(id),
+        createUserBoards: async (_, {id}, {dataSources, data}) => {
+            verficateAuthentication(data)
+            return await dataSources.favoriteboardAPI.createUser(id)
+        },
 
+        deleteUserBoards: async (_, {id}, {dataSources, data}) => {
+            verficateAuthentication(data)
+            return await dataSources.favoriteboardAPI.deleteUser(id)
+        },
 
-        createBoard: (_, {board}, {dataSources}) => dataSources.favoriteboardAPI.createBoard(board),
-        updateBoard: (_, {id, board}, {dataSources}) => dataSources.favoriteboardAPI.updateBoard(id, board),
-        deleteBoard: (_, {id}, {dataSources}) => dataSources.favoriteboardAPI.deleteBoard(id),
+        createBoard: async (_, {board}, {dataSources, data}) => {
+            verficateAuthentication(data)
+            return await dataSources.favoriteboardAPI.createBoard(board)
+        },
 
+        updateBoard: async (_, {id, board}, {dataSources, data}) => {
+            verficateAuthentication(data)
+            return await dataSources.favoriteboardAPI.updateBoard(id, board)
+        },
 
-        createUserFollow: (_, {userfollow}, {dataSources}) => dataSources.favoriteboardAPI.createUserFollow(userfollow),
-        deleteUserFollow: (_, {id}, {dataSources}) => dataSources.favoriteboardAPI.deleteUserFollow(id),
+        deleteBoard: async (_, {id}, {dataSources, data}) => {
+            verficateAuthentication(data)
+            return await dataSources.favoriteboardAPI.deleteBoard(id)
+        },
 
-        createBoardFollow: (_, {boardfollow}, {dataSources}) => dataSources.favoriteboardAPI.createBoardFollow(boardfollow),
-        deleteBoardFollow: (_, {id}, {dataSources}) => dataSources.favoriteboardAPI.deleteBoardFollow(id),
+        createUserFollow: async (_, {userfollow}, {dataSources, data}) => {
+            verficateAuthentication(data)
+            return await dataSources.favoriteboardAPI.createUserFollow(userfollow)
+        },
 
+        deleteUserFollow: async (_, {id}, {dataSources, data}) => {
+            verficateAuthentication(data)
+            return await dataSources.favoriteboardAPI.deleteUserFollow(id)
+        },
+
+        createBoardFollow: async (_, {boardfollow}, {dataSources, data}) => {
+            verficateAuthentication(data)
+            return await dataSources.favoriteboardAPI.createBoardFollow(boardfollow)
+        },
+
+        deleteBoardFollow: async (_, {id}, {dataSources, data}) => {
+            verficateAuthentication(data)
+            return await dataSources.favoriteboardAPI.deleteBoardFollow(id)
+        },
 
     },
 };
